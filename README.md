f7656dc86d2da75d1a553c0abd7bec4f20a0de83
mxBossard <mxbossard@protonmail.com>
2021-11-23 23:50:28 +0100

    Configure some go env var in goid mount a volume for gomodcache.

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 
 ## Setup go in Docker
-alias go="$PWD/goid.sh"
+ln -s ~/.local/bin/go $PWD/goid.sh
 
 ## Your first program
 see https://go.dev/doc/code
 
-
+## Using cobra
+go install github.com/spf13/cobra/cobra
+ln -s ~/.local/bin/ $PWD/bin/cobra
+export GOPATH=$PWD
+cobra
--- goid.sh
+++ goid.sh
@@ -1,9 +1,25 @@
-#! /bin/bash
+#! /bin/bash -e
 scriptDir=$( dirname $( readlink -f $0 ) )
 
-export GOBIN="./bin"
-
 ctDurationInSec=60
-ctId=$( docker run --rm -d --workdir=/root -e GOBIN="/root/$GOBIN" --volume=$scriptDir:/root:rw golang:1.17 sleep $ctDurationInSec )
+workDir=/tmp/goid
+
+export GOBIN="$workDir/bin"
+export GOCACHE="$workDir/.cache/go-build"
+export GOENV="$workDir/.config/go/env"
+export GOMODCACHE="/go/pkg/mod"
+
+localModCacheDir="$HOME/go/pkg/mod"
+mkdir -p "$localModCacheDir"
+
+runCmd="docker run --rm -d --user=$( id -u ):$( id -g ) --workdir=$workDir -e GOBIN -e GOCACHE -e GOENV -e GOMODCACHE --volume=$localModCacheDir:$GOMODCACHE:rw --volume=$scriptDir:$workDir:rw golang:1.17"
+#echo $runCmd
+
+ctId=$( $runCmd sleep $ctDurationInSec )
+#echo $ctId
+
+# Debug
+#docker exec -it $ctId "$@"
+#exit 0
 
 docker exec -it $ctId go "$@"
