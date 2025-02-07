58ac61abdcf0ee264708aa2660932356723932de
mxBossard <mxbossard@protonmail.com>
2025-02-07 02:04:28 +0100

    WIP cmdt: fix zcreen TailAllBlocking()

--- src/mby.fr/cmdtest/asyncCmdtTest.sh
+++ src/mby.fr/cmdtest/asyncCmdtTest.sh
@@ -109,15 +109,15 @@ $cmdtIn @init=meta3
 $cmdtIn @test=meta3/init @-- $newCmdt1 @init @async
 $cmdtIn @test=meta3/ @stderr= @-- $newCmdt1 true
 $cmdtIn @test=meta3/ @stderr= @-- $newCmdt1 true
-$cmdtIn @test=meta3/ @stderr:"Successfuly ran" @stderr!:"error" @stderr:"#01" @stderr:"#02" @stderr!:"#03" @-- $newCmdt1 @report=main
-$cmdtIn @test=meta3/ @fail @stderr:"$nothingToReportExpectedStderrMsg" @-- $newCmdt1 @token=$tk0 @report=main
+$cmdtIn @test=meta3/ @stderr:"Successfuly ran" @stderr!:"error" @stderr:"#01" @stderr:"#02" @stderr!:"#03" @-- $newCmdt1 @report=main @async
+$cmdtIn @test=meta3/ @fail @stderr:"$nothingToReportExpectedStderrMsg" @-- $newCmdt1 @token=$tk0 @report=main @async
 
 $cmdtIn @init=meta4
-$cmdtIn @test=meta4/init @-- $newCmdt1 @init @async
+$cmdtIn @test=meta4/init @-- $newCmdt1 @init=sub4 @async
 $cmdtIn @test=meta4/ @stderr= @-- $newCmdt1 @test=sub4/ true
 $cmdtIn @test=meta4/ @stderr= @-- $newCmdt1 @test=sub4/ true
-$cmdtIn @test=meta4/ @stderr:"Successfuly ran" @stderr!:"error" @stderr:"#01" @stderr:"#02" @stderr!:"#03" @-- $newCmdt1 @report=sub4
-$cmdtIn @test=meta4/ @fail @stderr:"$nothingToReportExpectedStderrMsg" @-- $newCmdt1 @report=sub4 @token=$tk0
+$cmdtIn @test=meta4/ @stderr:"Successfuly ran" @stderr!:"error" @stderr:"#01" @stderr:"#02" @stderr!:"#03" @-- $newCmdt1 @report=sub4 @async
+$cmdtIn @test=meta4/ @fail @stderr:"$nothingToReportExpectedStderrMsg" @-- $newCmdt1 @token=$tk0 @report=sub4 @async
 $cmdt @report 2>&1 | grep -v "Failures"
 
 export -n __CMDT_TOKEN
